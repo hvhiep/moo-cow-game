135df7f4b13cb3ef9982f917cfa755c9745aeeee
hvhiep14.8 <hvhiep14.8@gmail.com>
2025-08-18 00:01:36 +0700

    add gh-pages build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/moo-cow-game/", // Thay đổi thành tên repository của bạn
+  server: {
+    port: 8080,
+  },
+  build: {
+    outDir: "dist",
+  },
+});
